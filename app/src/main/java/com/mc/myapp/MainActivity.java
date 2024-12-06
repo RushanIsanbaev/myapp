e509f714419ec59d18bec073ae6f3f923107a28e
RushanIsanbaev <isanbaevrusha.1@gmail.com>
2024-12-06 17:13:15 +0500

    Третий коммит

--- app/src/main/java/com/mc/myapp/MainActivity.java
+++ app/src/main/java/com/mc/myapp/MainActivity.java
@@ -1,24 +1,21 @@
 package com.mc.myapp;
 
-import static android.icu.number.NumberRangeFormatter.with;
 import static java.math.BigDecimal.ONE;
 import static java.math.BigDecimal.valueOf;
 
 import android.os.Bundle;
-import android.util.Xml;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputEditText;
-import com.google.android.material.textfield.TextInputLayout;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-
 
 public class MainActivity extends AppCompatActivity {
     TextInputEditText priseEstate;
@@ -26,14 +23,14 @@ public class MainActivity extends AppCompatActivity {
     TextInputEditText interestRate;
     TextInputEditText term;
 
-    private TextView resLoanAmount ;
-    private TextView resMonthlyPayment ;
-    private TextView resRequiredIncome ;
-    private TextView resAmountOfInterest ;
+    private TextView resLoanAmount;
+    private TextView resMonthlyPayment;
+    private TextView resRequiredIncome;
+    private TextView resAmountOfInterest;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -58,41 +55,65 @@ public class MainActivity extends AppCompatActivity {
 
                 BigDecimal two = new BigDecimal("2");
                 BigDecimal twelve = new BigDecimal("12");
+                try { // это первое исключение
+                    if (priseEstate.getText().toString().equals("") |
+                            initialPayment.getText().toString().equals("") |
+                            interestRate.getText().toString().equals("") |
+                            term.getText().toString().equals("")) {
+                        throw new NumberFormatException();
+                    } else {
 
-                //5600000 1960000 10 30
+                        double priseEstateValue = Double.parseDouble(priseEstate.getText().toString());
+                        double initialPaymentValue = Double.parseDouble(initialPayment.getText().toString());
+                        double interestRateValue = Double.parseDouble(interestRate.getText().toString());
+                        double termValue = Double.parseDouble(term.getText().toString());
 
-                double priseEstateValue = Double.parseDouble(priseEstate.getText().toString());
-                double initialPaymentValue = Double.parseDouble(initialPayment.getText().toString());
-                double interestRateValue = Double.parseDouble(interestRate.getText().toString());
-                double termValue = Double.parseDouble(term.getText().toString());
+                        try {/* это второе он был вне первого, а после
+                        поэтому он продолжил выполняться дальше и попадался на исключительный случай
+                        при присваивании пустого поля к double (java.lang.NumberFormatException: empty String)*/
+                            if (priseEstateValue <= 0 | initialPaymentValue <= 0 | interestRateValue <= 0 | termValue <= 0) {
+                                throw new ArithmeticException();
+                            } else {
+                                BigDecimal priseEV = valueOf(priseEstateValue);
+                                BigDecimal initialPV = valueOf(initialPaymentValue);
+                                BigDecimal interestRV = valueOf(interestRateValue).divide(valueOf(100), 2, RoundingMode.HALF_UP);
+                                BigDecimal termV = valueOf(termValue).multiply(twelve).setScale(0, RoundingMode.HALF_UP);
 
-                BigDecimal priseEV = valueOf(priseEstateValue);
-                BigDecimal initialPV = valueOf(initialPaymentValue);
-                BigDecimal interestRV = valueOf(interestRateValue).divide(valueOf(100),2,RoundingMode.HALF_UP);
-                BigDecimal termV = valueOf(termValue).multiply(twelve).setScale(0,RoundingMode.HALF_UP);;
+                                BigDecimal loanAmount = priseEV.subtract(initialPV);//сумма кредита
+                                resLoanAmount.setText(String.valueOf(loanAmount.setScale(2, RoundingMode.HALF_UP)));
 
+                                BigDecimal iTR = interestRV.divide(twelve, 10, RoundingMode.HALF_UP);
+                                BigDecimal pow = ONE.add(iTR).pow(termV.intValue());
+                                BigDecimal tUp = iTR.multiply(pow).setScale(10, RoundingMode.HALF_UP);
+                                BigDecimal tDown = pow.subtract(ONE);
+                                BigDecimal divide = tUp.divide(tDown, 10, RoundingMode.HALF_UP);
+                                BigDecimal monthlyPayment = loanAmount.multiply(divide);// ежемесячный платёж
+                                resMonthlyPayment.setText(String.valueOf(monthlyPayment.setScale(2, RoundingMode.HALF_UP)));
 
-                BigDecimal loanAmount = priseEV.subtract(initialPV);//сумма кредита
-                resLoanAmount.setText(String.valueOf(loanAmount.setScale(2,RoundingMode.HALF_UP)));
+                                BigDecimal requiredIncome = monthlyPayment.multiply(two);// необходимый доход
+                                resRequiredIncome.setText(String.valueOf(requiredIncome.setScale(2, RoundingMode.HALF_UP)));
 
-                BigDecimal iTR = interestRV.divide(twelve,10,RoundingMode.HALF_UP);
-                BigDecimal pow = ONE.add(iTR).pow(termV.intValue());
-                BigDecimal tUp = iTR.multiply(pow).setScale(10,RoundingMode.HALF_UP);
-                BigDecimal tDown = pow.subtract(ONE);
-                BigDecimal divide = tUp.divide(tDown, 10, RoundingMode.HALF_UP);
-                BigDecimal monthlyPayment = loanAmount.multiply(divide);// ежемесячный платёж
-                resMonthlyPayment.setText(String.valueOf(monthlyPayment.setScale(2,RoundingMode.HALF_UP)));
+                                BigDecimal aOI = monthlyPayment.multiply(termV).setScale(10, RoundingMode.HALF_UP);
+                                BigDecimal amountOfInterest = aOI.subtract(loanAmount).setScale(2, RoundingMode.HALF_UP);//сумма процентов
+                                resAmountOfInterest.setText(String.valueOf(amountOfInterest));
+                            }
 
-                BigDecimal requiredIncome = monthlyPayment.multiply(two);// необходимый доход
-                resRequiredIncome.setText(String.valueOf(requiredIncome.setScale(2,RoundingMode.HALF_UP)));
-
-                BigDecimal aOI = monthlyPayment.multiply(termV).setScale(10,RoundingMode.HALF_UP);
-                BigDecimal amountOfInterest = aOI.subtract(loanAmount).setScale(2,RoundingMode.HALF_UP);//сумма процентов
-                resAmountOfInterest.setText(String.valueOf(amountOfInterest));
-
+                        } catch (ArithmeticException | NumberFormatException arithmeticException) {
+                            showInfoZero();
+                        }
+                    }
+                } catch (NumberFormatException exception) {
+                    showInfoEmpty();
+                }
             }
         });
     }
 
-}
+    private void showInfoZero() {
+        Toast.makeText(this, "Что - то пошло не так.\n\rПараметр не может быть 0.", Toast.LENGTH_LONG).show();
+    }
 
+    private void showInfoEmpty() {
+        Toast.makeText(this, "Что - то пошло не так.\n\rПоле не может быть пустым.", Toast.LENGTH_LONG).show();
+    }
+}
